a9a9c684be2ff6248de38ccaff9bfb98b0e3d5ad
sasacoh <sasacoh@71d67188-d948-0410-8b07-9980d9eb5191>
2008-03-20 08:49:10 +0000

    Optimize interfaces

--- trunk/SipekSdk/Common/AbstractWrapper.cs
+++ trunk/SipekSdk/Common/AbstractWrapper.cs
@@ -112,9 +112,126 @@ namespace Sipek.Common
     #endregion
   }
 
+  /// <summary>
+  /// 
+  /// </summary>
+  public interface ICallProxyInterface
+  {
+    #region Properties
+    int SessionId
+    { get; set; }
+    #endregion
 
-  #region Null pattern
+    #region Public Methods
 
+    int makeCall(string dialedNo, int accountId);
+
+    bool endCall();
+
+    bool alerted();
+
+    bool acceptCall();
+
+    bool holdCall();
+
+    bool retrieveCall();
+
+    bool xferCall(string number);
+
+    bool xferCallSession(int partnersession);
+
+    bool threePtyCall(int partnersession);
+
+    //bool serviceRequest(EServiceCodes code, int session);
+    bool serviceRequest(int code, string dest);
+
+    bool dialDtmf(string digits, int mode);
+
+    #endregion
+  }
+
+
+  #region Null Pattern
+
+  /// <summary>
+  /// 
+  /// </summary>
+  public class NullCallProxy : ICallProxyInterface
+  {
+    #region ICallProxyInterface Members
+
+    public int makeCall(string dialedNo, int accountId)
+    {
+      return 1;
+    }
+
+    public bool endCall()
+    {
+      return false;
+    }
+
+    public bool alerted()
+    {
+      return false;
+    }
+
+    public bool acceptCall()
+    {
+      return false;
+    }
+
+    public bool holdCall()
+    {
+      return false;
+    }
+
+    public bool retrieveCall()
+    {
+      return false;
+    }
+
+    public bool xferCall(string number)
+    {
+      return false;
+    }
+
+    public bool xferCallSession(int session)
+    {
+      return false;
+    }
+
+    public bool threePtyCall(int session)
+    {
+      return false;
+    }
+
+    public bool serviceRequest(int code, string dest)
+    {
+      return false;
+    }
+
+    public bool dialDtmf(string digits, int mode)
+    {
+      return false;
+    }
+
+    #endregion
+
+    #region ICallProxyInterface Members
+
+    public int SessionId
+    {
+      get { return 0; }
+      set { ; }
+    }
+
+    #endregion
+  }
+
+
+  /// <summary>
+  /// 
+  /// </summary>
   public class NullVoipProxy : IVoipProxy
   {
     #region ICommonProxyInterface Members
